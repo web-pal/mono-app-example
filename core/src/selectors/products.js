da7eddea1cde84a9097af210354d1aac1ba90dc5
VladimirPal <mail.vpal@gmail.dom>
2018-04-27 17:05:02 +0300

    feat: flow definition for variantsStringAttribute func

--- core/src/selectors/products.js
+++ core/src/selectors/products.js
@@ -1,14 +1,20 @@
 // @flow
+import type {
+  ProductResource,
+} from '../types';
 
 export const variantsStringAttribute = ({
   resource,
   showRaw = false,
   delimiter = ', ',
 }: {
-  resource: any,
+  resource: ProductResource,
   showRaw?: boolean,
   delimiter?: string,
-}) =>
-  resource.rl.productsVariants
-    .filter(p => showRaw || !p.attributes.isRaw)
-    .map(p => (p.attributes.name)).join(delimiter);
+}): string => (
+  resource.rl ?
+    resource.rl.productsVariants
+      .filter(p => showRaw || !p.attributes.isRaw)
+      .map(p => (p.attributes.name)).join(delimiter) :
+    ''
+);
