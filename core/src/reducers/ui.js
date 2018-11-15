369f77242fa0e0904cf56d9d3e9cdd329b9ec3e9
teorge <georgij.vozhdayenko@gmail.com>
2018-11-15 16:03:21 +0200

    feat(core): add actions for imageGallery and reducer which can deepmerge

--- core/src/reducers/ui.js
+++ core/src/reducers/ui.js
@@ -11,18 +11,91 @@ import type {
 
 const initialState: UiState = {
   randomString: 'Static string',
+  images: {
+    pixabayImages: [],
+    splashbaseImages: [],
+  },
+  selectedImage: {
+    pixabay: {
+      image: null,
+      index: null,
+    },
+    splashbase: {
+      image: null,
+      index: null,
+    },
+  },
+  error: null,
 };
 
+// simple universal ui reducer
+// export default ui = (
+//   state: UiState = initialState,
+//   action: Action,
+// ) => {
+//   switch (action.type) {
+//     case actionTypes.SET_UI_STATE:
+//       return {
+//         ...state,
+//         [action.payload.key]: action.payload.value,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const mergeUiValues = (
+  values,
+  deepMergeKeys,
+  state,
+) => (
+  Object.keys(values).reduce((s, v) => ({
+    ...s,
+    [v]: deepMergeKeys.includes(v)
+      ? {
+        ...state[v],
+        ...values[v],
+      }
+      : values[v],
+  }), {})
+);
+
+// universal ui reducer, which can get deep into a state tree
 const ui = (
   state: UiState = initialState,
   action: Action,
 ) => {
   switch (action.type) {
-    case actionTypes.SET_UI_STATE:
+    case actionTypes.SET_UI_STATE: {
+      const {
+        key,
+        values,
+      } = action.payload;
+      const { deepMergeKeys } = action.meta;
       return {
         ...state,
-        [action.payload.key]: action.payload.value,
+        ...(
+          values !== undefined
+            ? ({
+              [key]: {
+                ...state[key],
+                ...mergeUiValues(
+                  values,
+                  deepMergeKeys,
+                  state[key],
+                ),
+              },
+            })
+            : (
+              mergeUiValues(
+                key,
+                deepMergeKeys,
+                state,
+              )
+            )
+        ),
       };
+    }
     default:
       return state;
   }
